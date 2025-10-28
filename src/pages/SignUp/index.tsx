9caa632ab0222bee007d2ea00cd1f2c7cc60df54
ivanapana <s22310210@student.unklab.ac.id>
2025-10-28 23:09:09 +0800

    Adding SignUp Screen

---
+++ src/pages/SignUp/index.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import {StyleSheet, Text, View, ScrollView} from 'react-native';
+import TextInput from '../../components/molecules/TextInput';
+import Button from '../../components/atoms/Button';
+import Gap from '../../components/atoms/Gap';
+
+const SignUp = () => {
+  return (
+    <ScrollView contentContainerStyle={styles.container}>
+      <Text style={styles.title}>Sign Up</Text>
+
+      <View style={styles.form}>
+        <TextInput label="Full Name" placeholder="Type your full name" />
+        <Gap height={16} />
+        <TextInput
+          label="Email Address"
+          placeholder="Type your email address"
+        />
+        <Gap height={100} />
+        <Button label="Continue" />
+      </View>
+    </ScrollView>
+  );
+};
+
+export default SignUp;
+
+const styles = StyleSheet.create({
+  container: {
+    flexGrow: 1,
+    backgroundColor: 'white',
+    paddingHorizontal: 24,
+    paddingTop: 40,
+  },
+  title: {
+    fontFamily: 'Poppins-Medium',
+    fontSize: 22,
+    color: '#020202',
+    marginBottom: 24,
+  },
+  form: {
+    marginTop: 130,
+  },
+});
